745b39fd459545bd6f40d1dc2b2ff664826db825
Patryk Parafińczuk <patrykparafinczuk@gmail.com>
2020-07-14 18:22:23 +0200

    Added value pairs of tiles for later comparison

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const container = document.getElementById("inner-container");
 const startGameButton = document.getElementById("start-game-btn");
 const resetGameButton = document.getElementById("reset-game-btn");
+const valuePairs = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
 let isStarted = false;
 
 const startGame = () => {
+  if (isStarted) {
+    return;
+  }
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const tile = document.createElement("div");
       tile.classList.add("tiles");
-      tile.style.width = "80px";
-      tile.style.height = "80px";
-      tile.style.position = "absolute";
+      container.append(tile);
+
       tile.style.top = `${i * 90 + 25}px`;
       tile.style.left = `${j * 90 + 25}px`;
-      tile.style.backgroundColor = "black";
-      container.append(tile);
+
+      let idPair;
+      let isValidId = true;
+      while (isValidId) {
+        const positionSelected = Math.floor(Math.random() * 16);
+        idPair = valuePairs[positionSelected];
+        if (idPair !== "none") {
+          valuePairs[positionSelected] = "none";
+          isValidId = false;
+        }
+      }
+      tile.id = idPair;
     }
   }
   isStarted = true;
@@ -24,7 +37,10 @@ const resetGame = () => {
   if (!isStarted) {
     return;
   }
-
+  const tile = document.querySelectorAll(".tiles");
+  for (let i = 0; i < 16; i++) {
+    tile[i].removeAttribute("id");
+  }
 };
 
 startGameButton.addEventListener("click", startGame);
